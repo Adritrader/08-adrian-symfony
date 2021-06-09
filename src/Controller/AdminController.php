b5f7d324daf7850656cc968280024630047b3ba4
Adritrader <adri_denia_123@hotmail.com>
2021-06-09 13:38:01 +0200

    Productos tienda, Back/index, Back-productos

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Movie;
+use App\Entity\Producto;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+
+    public function index(): Response
+    {
+        $productoRepository = $this->getDoctrine()->getRepository(Producto::class);
+        $productos = $productoRepository->findAll();
+
+        if ($productos)
+        {
+            return $this->render('back/index.html.twig', ["productos"=>$productos]
+            );
+        }
+        else
+            return $this->render('back/index.html.twig', [
+                    'productos' => null]
+            );
+    }
+
+    /**
+     * @Route("/back-productos", name="back-productos")
+     */
+
+    public function backProductos(): Response
+    {
+        $productoRepository = $this->getDoctrine()->getRepository(Producto::class);
+        $productos = $productoRepository->findAll();
+
+        if ($productos)
+        {
+            return $this->render('back/back-productos.html.twig', ["productos"=>$productos]
+            );
+        }
+        else
+            return $this->render('back/back-productos.html.twig', [
+                    'productos' => null]
+            );
+    }
+
+    /**
+     * @Route("/back-productos/create", name="productos_create")
+     */
+    public function create(Request $request)
+    {
+        $producto = new Producto();
+        $form = $this->createForm(Producto::class, $producto);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $producto = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($producto);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('back/index.html.twig');
+        }
+        return $this->render('back/create-producto.html.twig', array(
+            'form' => $form->createView()));
+
+    }
+}
